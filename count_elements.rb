48b5e44a664f9fec89309f1aa23e603b598907da
Dan Kagan <dankagan7@gmail.com>
2019-05-14 02:03:54 +0000

    Automatically backed up by Learn

--- count_elements.rb
+++ count_elements.rb
@@ -2,7 +2,8 @@ require 'pry'
 
 def count_elements(array)
   new_hash = Hash.new(0)
-  array.each do |element| new_hash[element] += 1
+  array.each do |element| 
+    new_hash[element] += 1
   new_hash
   #binding.pry
   end

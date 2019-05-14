456fad3d60e5e0ed83eb767884ddbf7e97b28f10
Dan Kagan <dankagan7@gmail.com>
2019-05-14 02:19:26 +0000

    Automatically backed up by Learn

--- count_elements.rb
+++ count_elements.rb
@@ -2,9 +2,8 @@ require 'pry'
 
 def count_elements(array)
   array.each_with_object(Hash.new(0)) do |element, container| 
-  
+  binding.pry
     container[element] += 1
-    binding.pry
   end
 end
 

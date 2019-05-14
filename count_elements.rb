2cebe3480286365f189bbfbf7cef43e95a98146f
Dan Kagan <dankagan7@gmail.com>
2019-05-14 01:50:38 +0000

    Automatically backed up by Learn

--- count_elements.rb
+++ count_elements.rb
@@ -4,6 +4,7 @@ def count_elements(array)
   new_hash = {}
   array.each_with_object({}) do |key, container|
     if array.include?(key) 
+      counter += 1
       new_hash[key.to_sym]
     
     binding.pry

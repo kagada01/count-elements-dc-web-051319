54e7d77ae344e77ec74b5c93048fa130c766ce4f
Dan Kagan <dankagan7@gmail.com>
2019-05-14 01:50:00 +0000

    Automatically backed up by Learn

--- count_elements.rb
+++ count_elements.rb
@@ -3,7 +3,8 @@ require 'pry'
 def count_elements(array)
   new_hash = {}
   array.each_with_object({}) do |key, container|
-    if array.include?(key) new_hash.to_sym && 
+    if array.include?(key) 
+      new_hash.to_sym
     
     binding.pry
   end
